70963448a21664da56acfabb1d51bc79f435122d
JaviCallaghan <javicallaghan@hotmail.com>
2014-12-11 17:00:54 +0100

    * ADDED: Fancy javadoc

--- java8-new-features/src/main/java/es/jc/functions/BasicFunctions.java
+++ java8-new-features/src/main/java/es/jc/functions/BasicFunctions.java
@@ -14,6 +14,7 @@ import java.util.function.Supplier;
  * <li>{@link Predicate} - test(T t):boolean</li>
  * <li>{@link Supplier} - get():T</li>
  * </ul>
+ * <p><img src="../../../../resources/javadoc-files/java-logo-lambda.png"/></p>
  * 
  * @author JaviCallaghan
  */
